d3e5e5ffc2d744b0742eb2bd9cf68317a1ff1305
MAC OS <macos@MACs-MacBook-Pro.local>
2016-09-01 09:28:20 +0700

    first project commit

---
+++ pokedex-by-devslopesTests/pokedex_by_devslopesTests.swift
@@ -0,0 +1,36 @@
+//
+//  pokedex_by_devslopesTests.swift
+//  pokedex-by-devslopesTests
+//
+//  Created by MAC OS on 9/1/16.
+//  Copyright © 2016 Big Nerd Ranch. All rights reserved.
+//
+
+import XCTest
+@testable import pokedex_by_devslopes
+
+class pokedex_by_devslopesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
